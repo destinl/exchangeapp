f8690cc5d46ccd48380a2b2dd33e4c112b02cc28
destinl <1374214766@qq.com>
2024-09-26 08:27:18 +0800

    feat: 增加状态码、实现路由参数

--- controllers/article_controller.go
+++ controllers/article_controller.go
@@ -1,11 +1,13 @@
 package controllers
 
 import (
+	"errors"
 	"exchangeapp/global"
 	"exchangeapp/models"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 )
 
 func CreateArticle(ctx *gin.Context) {
@@ -33,9 +35,30 @@ func GetArticles(ctx *gin.Context) {
 	var articles []models.Article
 
 	if err := global.Db.Find(&articles).Error; err != nil {
-		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			ctx.JSON(http.StatusNotFound, gin.H{"error": "No articles found"})
+		} else {
+			ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		}
 		return
 	}
 
 	ctx.JSON(http.StatusOK, articles)
+}
+
+func GetArticleByID(ctx *gin.Context) {
+	id := ctx.Param("id")
+
+	var article models.Article
+
+	if err := global.Db.Where("id = ?", id).First(&article, id).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			ctx.JSON(http.StatusNotFound, gin.H{"error": "Article not found"})
+		} else {
+			ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		}
+		return
+	}
+
+	ctx.JSON(http.StatusOK, article)
 }
--- controllers/exchange_rate_controller.go
+++ controllers/exchange_rate_controller.go
@@ -1,12 +1,14 @@
 package controllers
 
 import (
+	"errors"
 	"exchangeapp/global"
 	"exchangeapp/models"
 	"net/http"
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 )
 
 func CreateExchangeRate(ctx *gin.Context) {
@@ -36,7 +38,11 @@ func GetExchangeRates(ctx *gin.Context) {
 	var exchangeRates []models.ExchangeRate
 
 	if err := global.Db.Find(&exchangeRates).Error; err != nil {
-		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			ctx.JSON(http.StatusNotFound, gin.H{"error": "Exchange rates not found"})
+		} else {
+			ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		}
 		return
 	}
 
